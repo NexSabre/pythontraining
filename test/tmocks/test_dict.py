cbec5b4fba7aa8c927f4780cbc8ec848adb31e15
Nex Sabre <NexSabre@users.noreply.github.com>
2024-12-18 23:06:08 +0100

    Add docstring to test_mock_dict for mocking os.environ

--- test/tmocks/test_dict.py
+++ test/tmocks/test_dict.py
@@ -1,6 +1,10 @@
 import os
 
 def test_mock_dict(mocker) -> None:
+    """
+    Mocking the os.environ dictionary.
+    Use mocker.patch.dict to mock a dictionary.
+    """
     environ_values = os.environ
     assert environ_values.get("KEY") is None
     mocker.patch.dict("os.environ", {"KEY": "VALUE1234567890"})
